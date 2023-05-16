744204ef029d851f0bcc5795311767c3747460bd
_ <85962372+N1ck120@users.noreply.github.com>
2023-05-16 00:03:05 +0000

    Added sudo and root verification

--- Script.sh
+++ Script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [[ $EUID -eq 0 ]]; then
+  current_user=$(logname)
+  home_dir=$(eval echo ~$current_user)
+else
+  home_dir=$HOME
+fi
+
 if ! pacman -Qi archiso > /dev/null 2>/dev/null; then
     yes|LC_ALL=en_US.UTF-8 sudo pacman -S archiso
 fi
@@ -10,13 +17,13 @@ echo "Select the desired unit ex: sdb"
 
 read usbdrive
 
-sudo mkarchiso -v -w /tmp/archiso-tmp -o $HOME/archiso/ /usr/share/archiso/configs/releng/
+sudo mkarchiso -v -w /tmp/archiso-tmp -o $home_dir/archiso/ /usr/share/archiso/configs/releng/
 
-cd $HOME/archiso/
+cd $home_dir/archiso/
 
 mv archlinux-"$(date +%Y.%m.%d)"-x86_64.iso arch.iso
 
-dd bs=4M if=$HOME/archiso/arch.iso of=/dev/$usbdrive conv=fsync oflag=direct status=progress
+dd bs=4M if=$home_dir/archiso/arch.iso of=/dev/$usbdrive conv=fsync oflag=direct status=progress
 
-sudo rm -rf /tmp/archiso-tmp $HOME/archiso
+sudo rm -rf /tmp/archiso-tmp $home_dir/archiso
 
